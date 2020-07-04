d4ac1101a76b5e7ea69d1721317e5dd6db99c135
yunju-kang <daria0155@naver.com>
2020-07-04 22:41:04 +0900

    issue chart

---
+++ crowling.py
@@ -0,0 +1,12 @@
+
+#Beautifulsoup 패키지 설치
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+with urlopen('https://search.naver.com/search.naver?sm=top_sug.pre&fbm=1&acr=3&acq=%EA%B2%80%EC%83%89%EC%96%B4&qdt=0&ie=utf8&query=%EB%84%A4%EC%9D%B4%EB%B2%84+%EC%8B%A4%EC%8B%9C%EA%B0%84+%EA%B2%80%EC%83%89%EC%96%B4+%EC%88%9C%EC%9C%84') as response:
+    soup = BeautifulSoup(response, 'html.parser')
+    grade = int(1)
+    for anchor in soup.select("span.tit"):
+        print("%d위: %s" %(grade, anchor.get_text()))
+        grade = grade + 1
+
